6d706af5784d3fee6ea33ca3c267e300527c6909
Joe <56809242+Jo3-L@users.noreply.github.com>
2021-06-03 10:14:15 -0700

    Use dcmd v3

--- stdcommands/dictionary/dictionary.go
+++ stdcommands/dictionary/dictionary.go
@@ -11,7 +11,7 @@ import (
 	"unicode"
 	"unicode/utf8"
 
-	"github.com/jonas747/dcmd/v2"
+	"github.com/jonas747/dcmd/v3"
 	"github.com/jonas747/discordgo"
 	"github.com/jonas747/yagpdb/bot/paginatedmessages"
 	"github.com/jonas747/yagpdb/commands"
